f6fa89fbdc491ea4f95ac4049aabee0dedad2108
Victor Quimbiamba <vicanall@gmail.com>
2026-02-04 17:12:45 -0500

    added empty main elements & layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Shell } from './layout/shell/shell';
+import { Home } from './pages/home/home';
+import { Clients } from './pages/clients/clients';
+import { Accounts } from './pages/accounts/accounts';
+import { Movements } from './pages/movements/movements';
+import { Reports } from './pages/reports/reports';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Shell,
+    children: [
+      { path: '', pathMatch: 'full', component: Home },
+      { path: 'clients', component: Clients },
+      { path: 'accounts', component: Accounts },
+      { path: 'movements', component: Movements },
+      { path: 'reports', component: Reports },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
