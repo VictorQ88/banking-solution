077c2ace14c81243aa92df7f6c6505b9bdf4d315
Victor Quimbiamba <vicanall@gmail.com>
2026-02-04 14:07:31 -0500

    put basic project on docker compose

--- BaseDatos.sql
+++ BaseDatos.sql
@@ -1,13 +1,23 @@
 CREATE TABLE clients (
-  id BIGSERIAL PRIMARY KEY,
-  name VARCHAR(120) NOT NULL,
-  gender VARCHAR(20) NOT NULL,
-  age INT NOT NULL,
-  identification VARCHAR(20) NOT NULL,
-  address VARCHAR(200) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  client_id VARCHAR(40) NOT NULL,
-  password VARCHAR(120) NOT NULL,
-  active BOOLEAN NOT NULL
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(120) NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    age INT NOT NULL,
+    identification VARCHAR(20) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    client_id VARCHAR(40) NOT NULL,
+    password VARCHAR(120) NOT NULL,
+    active BOOLEAN NOT NULL,
+
+    CONSTRAINT uk_clients_identification UNIQUE (identification),
+    CONSTRAINT uk_clients_client_id UNIQUE (client_id)
 );
 
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON TABLE clients
+TO banking_user;
+
+GRANT USAGE, SELECT
+ON SEQUENCE clients_id_seq
+TO banking_user;
