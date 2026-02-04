071526d4a7c89d135270d3e5902e703b4af2b775
Victor Quimbiamba <vicanall@gmail.com>
2026-02-04 11:24:25 -0500

    Client CRUD interface acction added

--- backend/src/main/java/com/banking/solution/service/ClientService.java
+++ backend/src/main/java/com/banking/solution/service/ClientService.java
@@ -1,4 +1,18 @@
 package com.banking.solution.service;
 
+import java.util.List;
+
+import com.banking.solution.dto.ClientDTO;
+
 public interface ClientService {
+
+    ClientDTO create(ClientDTO dto);
+
+    List<ClientDTO> findAll();
+
+    ClientDTO findById(Long id);
+
+    ClientDTO update(Long id, ClientDTO dto);
+
+    void delete(Long id);
 }
