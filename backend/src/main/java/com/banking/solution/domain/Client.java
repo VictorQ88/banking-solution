f94ef3290fb8e1b2fa42faddcb5c8abfb1d9ca7b
Victor Quimbiamba <vicanall@gmail.com>
2026-02-04 10:54:15 -0500

    Client added

---
+++ backend/src/main/java/com/banking/solution/domain/Client.java
@@ -0,0 +1,48 @@
+package com.banking.solution.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@NoArgsConstructor
+public class Client extends Person {
+
+    @NotBlank
+    @Size(max = 40)
+    @Column(name = "client_id", nullable = false, length = 40)
+    private String clientId;
+
+    @NotBlank
+    @Size(min = 6, max = 120)
+    @Column(nullable = false, length = 120)
+    private String password;
+
+    @NotNull
+    @Column(nullable = false)
+    private Boolean active;
+
+    public Client(
+            String name,
+            String gender,
+            Integer age,
+            String identification,
+            String address,
+            String phone,
+            String clientId,
+            String password,
+            Boolean active
+    ) {
+        super(null, name, gender, age, identification, address, phone);
+        this.clientId = clientId;
+        this.password = password;
+        this.active = active;
+    }
+}
