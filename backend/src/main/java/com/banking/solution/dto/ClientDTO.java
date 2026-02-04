2ce35613e45759c5eeba51763329c5e595c8fd40
Victor Quimbiamba <vicanall@gmail.com>
2026-02-04 11:21:06 -0500

    client DTo updated

--- backend/src/main/java/com/banking/solution/dto/ClientDTO.java
+++ backend/src/main/java/com/banking/solution/dto/ClientDTO.java
@@ -1,4 +1,48 @@
 package com.banking.solution.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class ClientDTO {
+
+    @NotBlank
+    @Size(max = 120)
+    public String name;
+
+    @NotBlank
+    @Size(max = 20)
+    public String gender;
+
+    @Min(18)
+    @Max(130)
+    public Integer age;
+
+    @NotBlank
+    @Size(max = 20)
+    public String identification;
+
+    @NotBlank
+    @Size(max = 200)
+    public String address;
+
+    @NotBlank
+    @Size(max = 20)
+    public String phone;
+
+    @NotBlank
+    @Size(max = 40)
+    public String clientId;
+
+    @NotBlank
+    @Size(min = 6, max = 120)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public String password;
+
+    @NotNull
+    public Boolean active;
 }
