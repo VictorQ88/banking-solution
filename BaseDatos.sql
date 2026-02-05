385c9b3252fd8f3fde8b9fbf6a16f93443cf879d
Victor Quimbiamba <vicanall@gmail.com>
2026-02-05 10:48:06 -0500

    new table added

--- BaseDatos.sql
+++ BaseDatos.sql
@@ -1,3 +1,4 @@
+-- Client
 CREATE TABLE clients (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
@@ -20,4 +21,27 @@ TO banking_user;
 
 GRANT USAGE, SELECT
 ON SEQUENCE clients_id_seq
+TO banking_user;
+
+-- Account
+
+-- Account
+CREATE TABLE accounts (
+    id BIGSERIAL PRIMARY KEY,
+    account_number VARCHAR(20) NOT NULL,
+    account_type VARCHAR(20) NOT NULL,
+    initial_balance NUMERIC(12,2) NOT NULL,
+    active BOOLEAN NOT NULL,
+    client_id BIGINT NOT NULL,
+
+    CONSTRAINT uk_accounts_account_number UNIQUE (account_number),
+    CONSTRAINT fk_accounts_client FOREIGN KEY (client_id) REFERENCES clients(id)
+);
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON TABLE accounts
+TO banking_user;
+
+GRANT USAGE, SELECT
+ON SEQUENCE accounts_id_seq
 TO banking_user;
